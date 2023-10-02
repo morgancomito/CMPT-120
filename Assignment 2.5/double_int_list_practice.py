72c1f970ff1a27ab25afc817adc9d66e0c08c4b8
morgancomito <morgan.comito1@marist.edu>
2023-10-01 22:29:12 -0400

    2.5

--- Assignment 2.5/double_int_list_practice.py
+++ Assignment 2.5/double_int_list_practice.py
@@ -1,29 +1,39 @@
 def main():
-  
-  #set this to any double
-  doubleValue =
-  
-  #set this to any int
-  intValue = 
-  
-  #print out addition, subtraction, multiplication, and division of these two values
-  
-  
-  
-  #populate this list
-  myFriends = []
-  
-  #print out your friends at index 2 and index 3
-  
-  
-  #populate this list with five numbers
-  fiveNumbers = []
-  
-  #do each of the four equations with different numbers each time.
-  
-  #now replace two of the numbers in the list with a different number (using name of list[x] = ?, not rewriting the fiveNumber list)
-  
-  #print out the list
-  
-  
+    # set this to any double
+    doubleValue = 1.4
+
+    # set this to any int
+    intValue = 6
+
+    # print out addition, subtraction, multiplication, and division of these two values
+    print(doubleValue + intValue)
+    print(intValue - doubleValue)
+    print(doubleValue * intValue)
+    print(intValue / doubleValue)
+
+    # populate this list
+    myFriends = ["Moira", "Riya", "Max", "Emma", "Hannah", "Luke"]
+
+    # print out your friends at index 2 and index 3
+    print(myFriends[2])
+    print(myFriends[3])
+
+    # populate this list with five numbers
+    fiveNumbers = [1, 5, 82, 900, 64]
+
+    # do each of the four equations with different numbers each time.
+    print(fiveNumbers[0] + fiveNumbers[1])
+    print(fiveNumbers[4] - fiveNumbers[3])
+    print(fiveNumbers[2] * fiveNumbers[1])
+    print(fiveNumbers[3] / fiveNumbers[0])
+
+    # now replace two of the numbers in the list with a different number (using name of list[x] = ?, not rewriting the
+    # fiveNumber list)
+    fiveNumbers[1] = 13
+    fiveNumbers[4] = 66
+
+    # print out the list
+    print(fiveNumbers)
+
+
 main()
--- Assignment 2.5/if_else_sign_practice.py
+++ Assignment 2.5/if_else_sign_practice.py
@@ -1,14 +1,22 @@
 def main():
 
-    '''ask the user for their age. If the user is 25 or older, tell them they can buy alcohol, nicotine products, and they can rent a car
-        If they're 21 or older but younger than 25, tell them they can buy alcohol and nicotine products, but cannot rent a car
+    """ask the user for their age. If the user is 25 or older, tell them they can buy alcohol, nicotine products, and
+    they can rent a car
+        If they're 21 or older but younger than 25, tell them they can buy alcohol and nicotine products, but cannot
+        rent a car
         If they're 18 and older but younger than 21, tell them they can only buy nicotine products in some states
         If they're less than 18, they can only purchase candy cigarettes and sody pops
-    '''
-    
-  
+    """
 
-  
-  
-  
+    age = int(input("What is your age?"))
+    if age >= 25:
+        print("You can buy alc, nic, and you can rent a car")
+    elif 25 > age >= 21:
+        print("You can buy alc and nic, but you can't rent a car")
+    elif 21 > age >= 18:
+        print("You can buy nic in some states")
+    else:
+        print("You can only buy candy cigarettes and sody pops")
+
+
 main()
