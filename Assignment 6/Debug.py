d947e42743bff800c1fbfd750c9b689cd194c79b
morgancomito <morgan.comito1@marist.edu>
2023-11-12 18:11:30 -0500

    A6

--- Assignment 6/Debug.py
+++ Assignment 6/Debug.py
@@ -1,67 +1,82 @@
 class Dog:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
 
 class Employee:
     def __init__(self, name, idNumber, department):
         self.name = name
         self.idNumber = idNumber
         self.department = department
-        
+
+
 class Cake:
-    #can you fill out the rest of this for me? im dumb
-    #the cake needs to have the cake flavor and cake frosting stored
+    def __init__(self, flavor, frosting):
+        # can you fill out the rest of this for me? im dumb
+        # the cake needs to have the cake flavor and cake frosting stored
+        self.flavor = flavor
+        self.frosting = frosting
+
 
 class Cat:
     def __init__(self, name, age, fur_length):
         self.name = name
         self.age = age
         self.fur_length = fur_length
-        
-        
+
     def breedGuess(self):
         if self.fur_length == "long":
-            return("Domestic Longhair")
+            return "Domestic Longhair"
         else:
-            return("Domestic Shorthair")
-            
+            return "Domestic Shorthair"
+
+
 class Car:
     def __init__(self, model, year, color):
         self.model = model
         self.year = year
         self.color = color
-        
-    #create your own function! what do you want it to do?
-    
-   
+
+    # create your own function! what do you want it to do?
+    def morgansCar(self):
+        if self.model == "Sentra" and self.year == 2010 and self.color == "gray":
+            return "Morgan's car"
+        else:
+            return "Not Morgan's car"
+
+
 def main():
-    #fill this one out with a dog's name and age.. can be your dog, friend's dog, etc
-    newDog = Dog(? , ?)
-    print(dog1.name, dog1.age)
-    
-    #and what about a new employee
-    newEmployee =
-    #how would we print out each of the variables from newEmployee?
-    print()
-    
-    #now create and print out a cake you make
-    
-    
-    
-    #and now create another cake and print it out
-    
-    
-    
-    #create a cat!
-    cat1 = Cat()
-    #create another cat!
-    
-    #What would we put here to print out the result of breedGuess for cat1?
-    print(cat1.?)
-    
-    #create a car!
-    
-    #Now print out the function you made for car :)
+    # fill this one out with a dog's name and age... can be your dog, friend's dog, etc
+    newDog = Dog("Carly", 13)
+    print(newDog.name, newDog.age)
+
+    # and what about a new employee
+    newEmployee = Employee("Morgan", 1, "clothing")
+    # how would we print out each of the variables from newEmployee?
+    print(newEmployee.name)
+    print(newEmployee.idNumber)
+    print(newEmployee.department)
+
+    # now create and print out a cake you make
+    cake1 = Cake("chocolate", "chocolate")
+    print("The cake is", cake1.flavor, "with", cake1.frosting, "frosting")
+
+    # and now create another cake and print it out
+    cake2 = Cake("ice cream", "whipped cream")
+    print("It is an", cake2.flavor, "cake with", cake2.frosting, "frosting")
+
+    # create a cat!
+    cat1 = Cat("Swiper", 10, "short")
+    # create another cat!
+    cat2 = Cat("Gabby", 20, "short")
+    # What would we put here to print out the result of breedGuess for cat1?
+    print(cat1.breedGuess())
+
+    # create a car!
+    car = Car("Sentra", 2010, "gray")
+    # Now print out the function you made for car :)
+    print(car.morgansCar())
+
 
 main()
--- Assignment 6/student.py
+++ Assignment 6/student.py
@@ -1,5 +1,5 @@
-#oo scary
-'''
+# oo scary
+"""
 Instructions:
 We're now experts at classes, right?
 Yeah?
@@ -16,23 +16,53 @@ create a function to see if the student is eligible for the honors program
 to be eligible they need to have a gpa above 3.5
 return true if they can and false if they cant, and print it out
 
-create a function because this college is a wacky one- every day they generate a student id and if the student id matches a student, that student gets free food that day. 
+create a function because this college is a wacky one- every day they generate a student id and if the student id
+matches a student, that student gets free food that day.
 1. generate a random number the length of however long you choose to make the id number
 2. compare if the random number matches your student's id
 3. if it matches print out "winner! student (name) gets free lunch!"
 4. if not, print "Loser!"
-(disclosure: obviously there's a very small chance of your generated number matching the student id number. I just want to see that you're generating and comparing properly)
-'''
+(disclosure: obviously there's a very small chance of your generated number matching the student id number. I just want
+to see that you're generating and comparing properly)
+"""
+from random import randint
+
 
 class student:
-    
-    
-    
-    
-    
-    
+    def __init__(self, name, studentID, year, major, gpa):
+        self.name = name
+        self.studentID = studentID
+        self.year = year
+        self.major = major
+        self.gpa = gpa
+
+    def honors(self):
+        if self.gpa > 3.5:
+            return True
+        else:
+            return False
+
+    def dailyGiveaway(self):
+        randomID = randint(10000000, 99999999)
+        if randomID == self.studentID:
+            return "Winner! Student {self.name} gets free lunch!"
+        else:
+            return "Loser!"
+
+
 def main():
-    #create three students and check if they get free lunch and if they qualify for honors
-    
-    
+    # create three students and check if they get free lunch and if they qualify for honors
+    Student1 = student("Morgan", 20127343, "junior", "math", 3.6)
+    Student2 = student("Dustin", 20141886, "sophomore", "sports comm", 2.9)
+    Student3 = student("Moira", 20129465, "junior", "poli sci", 3.4)
+
+    print(Student1.honors())
+    print(Student2.honors())
+    print(Student3.honors())
+
+    print(Student1.dailyGiveaway())
+    print(Student2.dailyGiveaway())
+    print(Student3.dailyGiveaway())
+
+
 main()
